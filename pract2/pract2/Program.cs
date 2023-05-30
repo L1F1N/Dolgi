46daccc53b42e76687975653b3b7a08086aae219
L1F1N <118181137+L1F1N@users.noreply.github.com>
2023-05-30 23:15:24 +0300

    Add files via upload

---
+++ pract2/pract2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double K = 0;
+            double a, b,t,c,d,m;
+            Console.WriteLine("Введите 't'");
+            t = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 'd'");
+            d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 'c'");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите 'm'");
+            m = Convert.ToDouble(Console.ReadLine());
+            a = (Math.Pow(t, 2) + d) / c;
+            b = t * Math.Cos(t) - m;
+            if (a>5)
+            {
+                K=Math.Pow(a, 2)+b;
+            }
+            if (a == 5)
+            {
+                K = a * Math.Cos(a);
+            }
+            if (a<5)
+            {
+                K = a - b;
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(K);
+        }
+    }
+}
---
+++ pract3/pract3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double H=0;
+            double x, u, y, Bk,i, B0, bshag;
+            Console.WriteLine("Введите x");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите u");
+            u = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B0");
+            B0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Bk");
+            Bk = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите bshag");
+            bshag = Convert.ToDouble(Console.ReadLine());
+            for (i=B0; i<Bk;i=i+bshag)
+            {
+                H=x*Math.Cos(u)-Math.Pow(y,u)-(Math.Sqrt(Math.Pow(u,2)+i*Math.Cos(i)))/Math.Tan(i);
+            }
+            Console.WriteLine(H);
+        }
+    }
+}
